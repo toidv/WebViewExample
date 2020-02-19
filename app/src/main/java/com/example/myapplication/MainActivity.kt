39b9fe41bf7e1ca30868f40d303859adc504810d
Steve Duong <duongvantoi@gmail.com>
2020-02-19 13:03:00 +0700

    Add example

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Message
+import android.util.Log
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.widget.FrameLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.webView)
+        val parentView = findViewById<FrameLayout>(R.id.flLayout)
+        webView.settings.lightTouchEnabled = true
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportMultipleWindows(true)
+        webView.loadUrl("file:///android_asset/www/index.html")
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onCreateWindow(
+                view: WebView,
+                dialog: Boolean,
+                userGesture: Boolean,
+                resultMsg: Message
+            ): Boolean {
+                Log.d("MainActivity", "oncreate window")
+                val newWebView = WebView(webView.context)
+                parentView.addView(newWebView)
+                val transport = resultMsg.obj as WebView.WebViewTransport
+                transport.webView = newWebView
+                resultMsg.sendToTarget()
+                return true
+            }
+        }
+    }
+}
+
